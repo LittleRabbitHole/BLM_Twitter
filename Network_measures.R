372fff8276bc1f376ad4e4a59de94b434498ae47
Angrrr_ <li.ang198744@gmail.com>
2018-02-03 13:56:57 -0500

    update
    update the globle atble

--- Network_measures.R
+++ Network_measures.R
@@ -10,7 +10,6 @@ data_08$V2 = as.character(data_08$V2)
 hashtages = unique(c(as.character(data_08$V1), as.character(data_08$V2)) )
 ID = 1:length(hashtages)
 hashtags_table = data.frame(ID, hashtages)
-
 #initialize global_tag_table as the first date
 global_tag_table = hashtags_table
 
@@ -30,28 +29,58 @@ global_tag_table = update_globaltags(data_09, global_tag_table)
 
 
 #merge global hashtable with IDs, output
-data_08_update = merge(data_08, hashtags_table, by.x = "V1", by.y = "hashtages")
-colnames(data_08_update) = c("V1", "V2", "V3", "V1_ID")
-data_08_update2 = merge(data_08_update, hashtags_table, by.x = "V2", by.y = "hashtages")
-colnames(data_08_update2) = c("V1", "V2", "V3", "V1_ID", "V2_ID")
-
-
-
-
-#building nets
-hashtag_net <- function(data){
-  #this function is to general the network
-  net = graph.edgelist(cbind(data$V1, data$V2), directed=FALSE) 
-  fullEdges = length(E(net))
-  V(net)$wpid = as.character(all_ids$wpid)
-  V(net)$label = as.character(all_ids$label)
-  V(net)$id = as.character(all_ids$ID)
-  V(net)$first_edit = as.character(all_ids$first_edit)
-  V(net)$from_event = all_ids$from_event
-  V(net)$final_check = all_ids$final_check
-  #session1: IGRAPH D-W- 16287 17321 --
-  E(session_net)$weight = session_edge$weight
-  
-  #results = session_net
-  return (session_net)
+update_edgelist <- function(edgelistdata, global_tag_table){
+  edgelistdata_update = merge(edgelistdata, global_tag_table, by.x = "V1", by.y = "hashtages")
+  colnames(edgelistdata_update) = c("V1", "V2", "V3", "V1_ID")
+  edgelistdata_update2 = merge(edgelistdata_update, global_tag_table, by.x = "V2", by.y = "hashtages")
+  colnames(edgelistdata_update2) = c("V1", "V2", "V3", "V1_ID", "V2_ID")
+  return(edgelistdata_update2)
 }
+
+edgelist08_update = update_edgelist(data_08, global_tag_table)
+
+
+
+edgelist = edgelist08_update
+#building nets
+hashtag_net <- function(edgelist, global_tag_table){
+  #this function is to general the network
+  net = graph.edgelist(cbind(edgelist$V1_ID, edgelist$V2_ID), directed=FALSE) 
+  fullEdges = length(E(net))
+  V(net)$label = as.character(global_tag_table$hashtages)
+  V(net)$id = as.character(global_tag_table$ID)
+  E(net)$weight = edgelist$V3
+  #results = session_net
+  return (net)
+}
+
+net_08 = hashtag_net(edgelist08_update, global_tag_table)
+summary(net_08)
+#IGRAPH U-W- 476 783 -- 
+#plot(net_08)
+
+network_output <- function(edge.net){
+  #this function is to calculate the individual network measures
+  wpid = V(edge.net)$wpid
+  SID = V(edge.net)$SID
+  netID =  V(edge.net)$id
+  transitivity = transitivity(edge.net, type="local")
+  betweenness = betweenness(edge.net,weights = E(edge.net)$weight, normalized = FALSE)
+  betweenness_norm = betweenness(edge.net,weights = E(edge.net)$weight, normalized = TRUE)
+  closeness = closeness(edge.net,weights = E(edge.net)$weight, normalized = FALSE)
+  closeness_norm = closeness(edge.net,weights = E(edge.net)$weight, normalized = TRUE)
+  degree = degree(edge.net, mode="all",normalized = FALSE)
+  degree_norm = degree(edge.net, mode="all",normalized = TRUE)
+  Indegree = degree(edge.net, mode="in",normalized = FALSE)
+  Indegree_norm = degree(edge.net, mode="in",normalized = TRUE)
+  Outdegree = degree(edge.net, mode="out",normalized = FALSE)
+  Outdegree_norm = degree(edge.net, mode="out",normalized = TRUE)
+  weighted_Indegree = strength(edge.net, mode = "in", weights = E(edge.net)$weight)
+  weighted_Outdegree = strength(edge.net, mode = "out", weights = E(edge.net)$weight)
+  eigen = eigen_centrality(edge.net, weights = E(edge.net)$weight, directed = TRUE)$vector
+  net_measure = cbind(netID, SID, wpid, transitivity, betweenness, betweenness_norm, 
+                      closeness, closeness_norm, degree, degree_norm, Indegree, Indegree_norm, 
+                      Outdegree, Outdegree_norm, weighted_Indegree, weighted_Outdegree, eigen) 
+  return (net_measure)
+}
+
