d7b530afd651dd359f685bc4834db23122c1a55d
Angrrr_ <li.ang198744@gmail.com>
2018-02-10 10:55:25 -0500

    update

--- Network_measures.R
+++ Network_measures.R
@@ -5,10 +5,7 @@ source('Network_Functions.R')
 setwd("/Users/angli/ANG/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/Yu-Ru_Project_GDrive/2017_blacklivesmatter/data/extracted_graph")
 library(igraph)
 data_08 = read.csv("08_08.csv", header = FALSE)
-data_08$V1 = trimws(data_08$V1)
-data_08$V1 = as.character(data_08$V1)
-data_08$V2 = trimws(data_08$V2)
-data_08$V2 = as.character(data_08$V2)
+data_08 = pre_process(data_08)
 
 #need to keep the id/tashtag unique, maintain a name table
 #updating for each day
@@ -21,22 +18,27 @@ global_tag_table = hashtags_table
 edgelist08_update = update_edgelist(data_08, global_tag_table)
 net_08 = hashtag_net(edgelist08_update, global_tag_table)
 summary(net_08)
-#IGRAPH U-W- 330 783 -- 
+
 network_measure_08 = network_output(net_08)
 write.csv(network_measure_08, "network_measure_08.csv")
 write_graph(net_08, "net_08.graphml", format ="graphml")
 
 
-
-
-#updating the global_tag_table with new date
+#updating the global_tag_table with new date 08/09
 data_09 = read.csv("08_09.csv", header = FALSE)
+data_09 = pre_process(data_09)
+#updating new global table
 global_tag_table = update_globaltags(data_09, global_tag_table)
+#edge number should be same as the data import edgelist number
+edgelist09_update = update_edgelist(data_09, global_tag_table)
+net_09 = hashtag_net(edgelist09_update, global_tag_table)
+summary(net_09)
+network_measure_09 = network_output(net_09)
+write.csv(network_measure_09, "network_measure_09.csv")
+write_graph(net_09, "net_09.graphml", format ="graphml")
 
-edgelist08_update = update_edgelist(data_08, global_tag_table)
 
-
-
+#############################################
 edgelist = edgelist08_update
 #building nets
 hashtag_net <- function(edgelist, global_tag_table){
