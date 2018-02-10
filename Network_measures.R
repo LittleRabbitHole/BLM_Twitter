ca2f4607f6d43850641243df16e0f76a0e03c3bb
Angrrr_ <li.ang198744@gmail.com>
2018-02-10 10:12:06 -0500

    separate out functions

--- Network_measures.R
+++ Network_measures.R
@@ -1,7 +1,13 @@
+setwd("/Users/angli/Documents/GitHub/BLM_Twitter")
+
+source('Network_Functions.R')
+
 setwd("/Users/angli/ANG/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/Yu-Ru_Project_GDrive/2017_blacklivesmatter/data/extracted_graph")
 library(igraph)
 data_08 = read.csv("08_08.csv", header = FALSE)
+data_08$V1 = trimws(data_08$V1)
 data_08$V1 = as.character(data_08$V1)
+data_08$V2 = trimws(data_08$V2)
 data_08$V2 = as.character(data_08$V2)
 
 #need to keep the id/tashtag unique, maintain a name table
@@ -12,30 +18,20 @@ ID = 1:length(hashtages)
 hashtags_table = data.frame(ID, hashtages)
 #initialize global_tag_table as the first date
 global_tag_table = hashtags_table
+edgelist08_update = update_edgelist(data_08, global_tag_table)
+net_08 = hashtag_net(edgelist08_update, global_tag_table)
+summary(net_08)
+#IGRAPH U-W- 330 783 -- 
+network_measure_08 = network_output(net_08)
+write.csv(network_measure_08, "network_measure_08.csv")
+write_graph(net_08, "net_08.graphml", format ="graphml")
+
+
+
 
 #updating the global_tag_table with new date
 data_09 = read.csv("08_09.csv", header = FALSE)
-update_globaltags <- function(new_data, global_tag_table){
-  #updating the global_tag_table with new date
-  hashtages_new = unique(c(as.character(new_data$V1), as.character(new_data$V2)) )
-  new = setdiff(global_tag_table$hashtages, hashtages_new) #new hashtags
-  newIDs = (length(global_tag_table$hashtages)+1):(length(global_tag_table$hashtages)+length(new))
-  new_tage_table = data.frame(ID = newIDs, hashtages = new)
-  global_tag_table = rbind(global_tag_table, new_tage_table)
-  return(global_tag_table)
-}
-
 global_tag_table = update_globaltags(data_09, global_tag_table)
-
-
-#merge global hashtable with IDs, output
-update_edgelist <- function(edgelistdata, global_tag_table){
-  edgelistdata_update = merge(edgelistdata, global_tag_table, by.x = "V1", by.y = "hashtages")
-  colnames(edgelistdata_update) = c("V1", "V2", "V3", "V1_ID")
-  edgelistdata_update2 = merge(edgelistdata_update, global_tag_table, by.x = "V2", by.y = "hashtages")
-  colnames(edgelistdata_update2) = c("V1", "V2", "V3", "V1_ID", "V2_ID")
-  return(edgelistdata_update2)
-}
 
 edgelist08_update = update_edgelist(data_08, global_tag_table)
 
