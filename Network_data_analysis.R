493d4bdad762da4316adb8eab0756a9b7ec86b1e
Angrrr_ <li.ang198744@gmail.com>
2018-02-24 13:21:42 -0500

    udpate

--- Network_data_analysis.R
+++ Network_data_analysis.R
@@ -5,31 +5,44 @@ library(ggplot2)
 
 setwd("/Users/angli/ANG/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/Yu-Ru_Project_GDrive/2017_blacklivesmatter/data/extracted_graph/network_graph")
 
-#read the graph data
-net = read_graph("net_08.graphml", format = "graphml")
-summary(net)
-LabelID = data.frame(V(net)$id, V(net)$label)
-colnames(LabelID) = c("id", "label")
+filtered_net <- function(net){
+  #read the graph data
+  #summary(net)
+  LabelID = data.frame(V(net)$id, V(net)$label)
+  colnames(LabelID) = c("id", "label")
 
-#backdone filtering out unimportant edges
-net_filtered = backbone(net)
-id = min(c(net_filtered$from, net_filtered$to)):max(c(net_filtered$from, net_filtered$to))
-ID_filter = data_frame(id)
-filtered_IDlabel = merge(ID_filter, LabelID, by = "id")
-
-#form updated network graph
-net_new = graph.edgelist(cbind(net_filtered$from, net_filtered$to), directed=FALSE) 
-summary(net_new)
-E(net_new)$weight = net_filtered$weight
-V(net_new)$label = as.character(filtered_IDlabel$label)
+  #backdone filtering out unimportant edges
+  net_filtered = backbone(net)
+  id = 1:max(c(net_filtered$from, net_filtered$to))
+  ID_filter = data_frame(id)
+  filtered_IDlabel = merge(ID_filter, LabelID, by = "id")
   
-net_new1 =  simplify(net_new)
-net_new2 = delete_vertices(net_new1, igraph::degree(net_new1)==0)
+  #form updated network graph
+  net_new = graph.edgelist(cbind(net_filtered$from, net_filtered$to), directed=FALSE) 
+  #summary(net_new)
+  E(net_new)$weight = net_filtered$weight
+  V(net_new)$label = as.character(filtered_IDlabel$label)
+  
+  net_new1 =  simplify(net_new)
+  net_new2 = delete_vertices(net_new1, igraph::degree(net_new1)==0)
 
-# the coreness
-#coreness(net_new2, mode = "all")
-#length(coreness(net_08_new2, mode = "all"))
-V(net_new2)$kcore = coreness(net_new2, mode = "all")
-#summary(net_08)
+  # the coreness
+  #coreness(net_new2, mode = "all")
+  #length(coreness(net_08_new2, mode = "all"))
+  V(net_new2)$kcore = coreness(net_new2, mode = "all")
+  summary(net_new2)
+  
+  return(net_new2)
+  
+}
 
-plot(net_new2, edge.width=0.05*E(net_new2)$weight)
+net = read_graph("net_10.graphml", format = "graphml")
+summary(net)
+net_new = filtered_net(net)
+write_graph(net_new, "newnet_10.graphml", format ="graphml")
+table(V(net_new)$kcore)
+#coreness
+V(net_new)$label[V(net_new)$kcore == 4]
+plot(igraph::subgraph(net_new, V(net_new)$kcore == 3))
+
+plot(net_new, edge.width=0.05*E(net_new2)$weight, layout=layout.graphopt)
